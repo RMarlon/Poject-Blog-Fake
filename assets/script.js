1d07510fe902e665d6bceee6cc71a7c1a2bc4f90
Marlon Santos <xsantos.marlonx@gmail.com>
2024-04-22 10:58:54 -0300

    conclued the project blog fake

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,57 @@
 //'https://jsonplaceholder.typicode.com/posts'
 
-async function readPoats(){
+async function readPoats() {
     let postsArea = document.querySelector('.posts');
     postsArea.innerHTML = 'Loading...';
 
     let response = await fetch('https://jsonplaceholder.typicode.com/posts');
     let json = await response.json();
 
-    if(json.length > 0){
+    if (json.length > 0) {
         postsArea.innerHTML = '';
-        for(let i in json){
+        for (let i in json) {
             let postHtml = `<div><h1>${json[i].title}</h1>${json[i].body}<hr/></div>`;
             postsArea.innerHTML += postHtml;
         }
     }
-    else{
-        postsArea.innerHTML = 'No post to display'
+    else {
+        postsArea.innerHTML = 'No post to display';
     }
 }
+
+async function addNewPost(title, body) {
+    await fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+        {
+            method: 'Post',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                body,
+                userId: 2
+            })
+        }
+    );
+
+    document.querySelector('#titleField').value = '';
+    document.querySelector('#bodyField').value = '';
+
+    readPoats();
+}
+
+document.querySelector('#insertButton').addEventListener('click', () => {
+    let title = document.querySelector('#titleField').value;
+    let body = document.querySelector('#bodyField').value;
+
+    if (title && body) {
+        addNewPost(title, body);
+    }
+    else {
+        alert('Fill in all the fields');
+    }
+
+});
 
 readPoats()
